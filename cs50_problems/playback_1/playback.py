d335c187749b32c39db88daf41cdb761f6fc1a8f
Trisztan <trisztanfarkas123@gmail.com>
2024-09-15 16:04:05 +0200

    deleted file

--- cs50_problems/playback_1/playback.py
+++
@@ -1,7 +0,0 @@
-while True:
-    message = input("Write something here: ")
-    modified_message = message.replace(" ", "...")
-    print(modified_message)
-    option = input("Would you like to quit? y/n ").lower()
-    if option == "y":
-        break
