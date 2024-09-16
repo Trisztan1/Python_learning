811a4afcc70172c76df20a9568d9eb363324746a
Trisztan <trisztanfarkas123@gmail.com>
2024-09-16 22:36:47 +0200

    meal cs50p task

---
+++ cs50_problems/meal/meal.py
@@ -0,0 +1,25 @@
+def main():
+    ...
+    user_input = input("What time is it? ")
+    time = convert(user_input)
+    if 18 < time < 19:
+        print("dinner time")
+    elif 12 < time < 13:
+        print("lunch time")
+    elif 7 < time < 8:
+        print("breakfast time")
+   
+
+
+def convert(time):
+    ...
+    x,y = time.split(":")
+    x = int(x)
+    y = float(y)
+    y = y / 60
+    return x + y
+    
+
+
+if __name__ == "__main__":
+    main()
