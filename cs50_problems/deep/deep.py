fd00a51fd25ef08c5701391f9b6f64effdc50f74
Trisztan <trisztanfarkas123@gmail.com>
2024-09-16 11:42:24 +0200

    with changes forty two is also yes

--- cs50_problems/deep/deep.py
+++ cs50_problems/deep/deep.py
@@ -1,7 +1,7 @@
-question = input("What is the Answer to the Great Question of Life, the Universe and Everything? ").strip()
+question = input("What is the Answer to the Great Question of Life, the Universe and Everything? ").strip().lower()
 
 match question:
-    case "42" | "Forty Two" | "forty-two":
+    case "42" | "Forty Two" | "forty-two" | "forty two":
         print("Yes")
     case _:
         print("No")
