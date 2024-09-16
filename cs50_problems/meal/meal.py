911ef1d0bc7972b25137deaa6ad0d60d0a6a91e5
Trisztan <trisztanfarkas123@gmail.com>
2024-09-16 22:42:19 +0200

    added greater or equal

--- cs50_problems/meal/meal.py
+++ cs50_problems/meal/meal.py
@@ -2,11 +2,11 @@ def main():
     ...
     user_input = input("What time is it? ")
     time = convert(user_input)
-    if 18 < time < 19:
+    if 18 <= time < 19:
         print("dinner time")
-    elif 12 < time < 13:
+    elif 12 <= time < 13:
         print("lunch time")
-    elif 7 < time < 8:
+    elif 7 <= time < 8:
         print("breakfast time")
    
 
