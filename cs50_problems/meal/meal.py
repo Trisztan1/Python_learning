245d1aabe69df8c8226b6c89f16a11b748eb329d
Trisztan <trisztanfarkas123@gmail.com>
2024-09-17 01:26:25 +0200

    rewrited x to hours and y to minutes

--- cs50_problems/meal/meal.py
+++ cs50_problems/meal/meal.py
@@ -2,11 +2,11 @@ def main():
     ...
     user_input = input("What time is it? ")
     time = convert(user_input)
-    if 18 <= time < 19:
+    if 18 <= time <= 19:
         print("dinner time")
-    elif 12 <= time < 13:
+    elif 12 <= time <= 13:
         print("lunch time")
-    elif 7 <= time < 8:
+    elif 7 <= time <= 8:
         print("breakfast time")
    
 
@@ -14,10 +14,10 @@ def main():
 def convert(time):
     ...
     x,y = time.split(":")
-    x = int(x)
-    y = float(y)
-    y = y / 60
-    return x + y
+    hours = int(x)
+    minutes = float(y)
+    minutes = minutes / 60
+    return hours + minutes
     
 
 
