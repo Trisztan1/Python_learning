2847bd5cb925e3e17afff32aa9d35d7829e136f4
Trisztan <trisztanfarkas123@gmail.com>
2024-09-19 16:09:20 +0200

    updated the two files, the output is both upper and lower case

--- cs50_problems/twttr/twttr.py
+++ cs50_problems/twttr/twttr.py
@@ -1,17 +1,17 @@
 def main():
-    user_input = input("Input: ").lower().strip()
+    user_input = input("Input: ").strip()
     print(trimm_vowels(user_input))
 
 
 
 def trimm_vowels(user_input):
-    excluded_vowels = ["a", "e", "i", "o", "u"]
+    excluded_vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
     output = []
     for letter in user_input:
         if letter in excluded_vowels:
             continue
         else:
             output.append(letter)
-    return "".join(output).capitalize()
+    return "".join(output)
 
 main()
--- cs50_problems/twttr/twttr_1.1.py
+++ cs50_problems/twttr/twttr_1.1.py
@@ -1,10 +1,10 @@
 def main():
-    user_input = input("Input: ").lower().strip()
+    user_input = input("Input: ").strip()
     print(trim_vowels(user_input))
 
 def trim_vowels(user_input):
-    excluded_vowels = {"a", "e", "i", "o", "u"}
+    excluded_vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
     output = [letter for letter in user_input if letter not in excluded_vowels]
-    return "".join(output).capitalize()
+    return "".join(output)
 
 main()
