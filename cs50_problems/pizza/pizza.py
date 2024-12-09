09a66558a0d0fa1ed2068d9e11372b088cdc8cd9
Trisztan <trisztanfarkas123@gmail.com>
2024-12-09 19:07:09 +0100

    Add main guard to pizza.py for better module usage

--- cs50_problems/pizza/pizza.py
+++ cs50_problems/pizza/pizza.py
@@ -2,10 +2,10 @@ import sys
 from tabulate import tabulate
 import csv
 
+
 def main():
     check_argv(sys.argv)
     print_ASCII_menu(sys.argv[1])
-
 
 
 def check_argv(args):
@@ -14,7 +14,7 @@ def check_argv(args):
         sys.exit("Too many command-line arguments")
     elif len(args) < 2:
         sys.exit("Too few command-line arguments")
-    
+
     found = any(args[1].endswith(ext) for ext in extensions)
     if not found:
         sys.exit("Not a CSV file")
@@ -28,16 +28,16 @@ def check_argv(args):
     # if not found:
     #     sys.exit("Not a CSV file")
 
+
 def print_ASCII_menu(arg):
     menu = []
     with open(arg, "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
             menu.append(row)
-    
+
     print(tabulate(menu, headers="keys", tablefmt="grid"))
 
-            
 
-
-main()
+if __name__ == "__main__":
+    main()
